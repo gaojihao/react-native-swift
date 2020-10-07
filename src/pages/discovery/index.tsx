d2f00baa1aceb234f71dc404ba626bde9a2e80bc
lizhi <zhi.li@wz-inc.com>
2020-10-07 17:46:44 +0800

    add files

---
+++ src/pages/discovery/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.less'
+
+export default class Discovery extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='discovery'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
