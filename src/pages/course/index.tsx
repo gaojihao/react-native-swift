b10afbb9b422ef85a3afb55f132bfe9f60ba8116
lizhi <zhi.li@wz-inc.com>
2020-10-08 17:49:13 +0800

    add files

---
+++ src/pages/course/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.less'
+
+export default class Course extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='course'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
---
+++ src/pages/play/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.less'
+
+export default class Play extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='play'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
