9919a9375d6f6bb004072a5e3db3ab44d5ef8f70
lizhi <lizhi1026@126.com>
2023-04-15 08:41:54 +0800

    init

---
+++ react-native-swift/react-native-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  react-native-swift
+//
+//  Created by Aaron on 2023/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+    }
+
+
+}
+
